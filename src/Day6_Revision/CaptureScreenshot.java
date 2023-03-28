b6f755f398d11aa48923d515c9f9221978001681
PeterFikry <peterfikry11@gmail.com>
2023-03-28 10:09:30 -0400

    class6

---
+++ src/Day6_Revision/CaptureScreenshot.java
@@ -0,0 +1,29 @@
+package Day6_Revision;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CaptureScreenshot {
+    public static void main(String[] args) throws IOException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://www.salesforce.com/au/");
+
+        //Take the screenshot and save to a file
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File src =ts.getScreenshotAs(OutputType.FILE);
+
+        //Save the screenshot to a specified location
+        File target=new File("/Users/peterghobrial/Desktop/peter.jpg");
+        FileUtils.copyFile(src,target);
+    }
+}
